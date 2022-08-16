82b23aafc71705b51fb1588cfcb79c0fecb3efa1
Casey <doesnotsitproperly@gmail.com>
2022-08-16 09:28:20 -0400

    Rename colors_*.tres files

--- scripts/Global.cs
+++ scripts/Global.cs
@@ -8,14 +8,14 @@ public class Global : Node {
 
     public static SpatialMaterial RandomColor() {
         String[] colors = new String[] {
-            "res://resources/sweetie_16/color_2.tres",
-            "res://resources/sweetie_16/color_3.tres",
-            "res://resources/sweetie_16/color_4.tres",
-            "res://resources/sweetie_16/color_5.tres",
-            "res://resources/sweetie_16/color_6.tres",
-            "res://resources/sweetie_16/color_7.tres",
-            "res://resources/sweetie_16/color_8.tres",
-            "res://resources/sweetie_16/color_9.tres",
+            "res://resources/sweetie_16/color_02.tres",
+            "res://resources/sweetie_16/color_03.tres",
+            "res://resources/sweetie_16/color_04.tres",
+            "res://resources/sweetie_16/color_05.tres",
+            "res://resources/sweetie_16/color_06.tres",
+            "res://resources/sweetie_16/color_07.tres",
+            "res://resources/sweetie_16/color_08.tres",
+            "res://resources/sweetie_16/color_09.tres",
             "res://resources/sweetie_16/color_10.tres",
             "res://resources/sweetie_16/color_11.tres",
             "res://resources/sweetie_16/color_12.tres",
