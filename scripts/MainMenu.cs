faf1d82549c536ee9b8ee7def01684d1905b235e
Casey <doesnotsitproperly@gmail.com>
2022-08-26 15:13:11 -0400

    Add licenses and exit button

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -2,17 +2,38 @@ using Godot;
 using System;
 
 public class MainMenu : Node2D {
+    private Button exit;
     private Sprite sprite;
 
     public override void _Ready() {
+        exit = GetNode<Button>("ExitButton");
         sprite = GetNode<Sprite>("Sprite");
+
+        if (OS.GetName() == "HTML5") {
+            exit.QueueFree();
+        }
     }
 
     public override void _Process(Single delta) {
         sprite.RotationDegrees += 1f;
     }
 
-    public void OnButtonPressed() {
+    public void OnStartButtonPressed() {
         GetTree().ChangeScene("res://scenes/main.tscn");
+    }
+
+    public void OnLicensesButtonPressed() {
+        if (OS.GetName() == "HTML5") {
+            OS.ShellOpen("https://github.com/doesnotsitproperly/cube-prison-2/blob/main/THIRD_PARTY_LICENSES.txt");
+        } else {
+            Error error = OS.ShellOpen(OS.GetExecutablePath().GetBaseDir() + "/THIRD_PARTY_LICENSES.txt");
+            if (error != Error.Ok) {
+                OS.ShellOpen("https://github.com/doesnotsitproperly/cube-prison-2/blob/main/THIRD_PARTY_LICENSES.txt");
+            }
+        }
+    }
+
+    public void OnExitButtonPressed() {
+        GetTree().Notification(NotificationWmQuitRequest);
     }
 }
