93dbe8c77577cf03a497875e4f24841157ec2496
Casey <doesnotsitproperly@gmail.com>
2022-08-26 07:47:31 -0400

    Auto HTML5 export test 5

--- build.ps1
+++ build.ps1
@@ -8,17 +8,13 @@ $templatesDir = Join-Path $PSScriptRoot "templates"
 $godotZip = Join-Path $PSScriptRoot "godot.zip"
 $godotDir = Join-Path $PSScriptRoot "godot"
 
-cd ~
-Write-Output (Get-Location)
-
-<#
 # Get export templates
 
 Invoke-WebRequest "https://github.com/godotengine/godot/releases/download/3.5-stable/Godot_v3.5-stable_mono_export_templates.tpz" -OutFile $templatesTpz
 Expand-Archive $templatesTpz -DestinationPath $templatesDir
 
-Copy-Item (Join-Path $templatesDir "templates" "webassembly_debug.zip") -Destination "/home/runner/.local/share/godot/templates/3.5.stable.mono/"
-Copy-Item (Join-Path $templatesDir "templates" "webassembly_release.zip") -Destination "/home/runner/.local/share/godot/templates/3.5.stable.mono/"
+Copy-Item (Join-Path $templatesDir "templates" "webassembly_debug.zip") -Destination "~/.local/share/godot/templates/3.5.stable.mono/"
+Copy-Item (Join-Path $templatesDir "templates" "webassembly_release.zip") -Destination "~/.local/share/godot/templates/3.5.stable.mono/"
 
 # Build
 
@@ -30,4 +26,3 @@ New-Item $buildDir -ItemType "directory" | Out-Null
 Rename-Item (JOin-Path $PSScriptRoot "html5_export.cfg") -NewName "export_presets.cfg"
 
 ./godot/Godot_v3.5-stable_mono_linux_headless_64/Godot_v3.5-stable_mono_linux_headless.64 --no-window --export "HTML5" (Join-Path $buildDir "index.html")
-#>
